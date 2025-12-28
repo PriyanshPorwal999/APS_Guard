c9c741c40415518517ade668b6b04064a45ded14
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2025-12-28 11:53:50 +0530

    Initialize main file

---
+++ main.py
@@ -0,0 +1,21 @@
+from sensor.exception import SensorException
+import sys
+import os
+
+from sensor.logger import logging
+
+
+def test_exception():
+    
+    try:
+        logging.info("Hi, Error aane wali hai named as division by zero wali")
+        a = 1/0
+    except Exception as e:
+        raise SensorException(e, sys)  
+
+
+if __name__ == "__main__":
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
