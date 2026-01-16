332bfccb6d1332e3353c449567654146506d5c35
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2026-01-16 19:57:58 +0530

    Update get requirement func., accepting .env file

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
 from setuptools import find_packages, setup
 from typing import List
 
-def get_requirements()->List[str]:
-    requirements_list: list[str] = []
+# def get_requirements()->List[str]:
+#     requirements_list: list[str] = []
 
-    return requirements_list
+#     return requirements_list
+
+# def get_requirements() -> List[str]:
+#     requirements_list: List[str] = []
+
+#     with open("requirements.txt") as file:
+#         requirements_list = file.read().splitlines()
+
+#     return requirements_list
+
+
+def get_requirements() -> List[str]:
+    requirements = []
+    with open("requirements.txt") as f:
+        for line in f:
+            line = line.strip()
+            if line and not line.startswith("-e"):
+                requirements.append(line)
+    return requirements
 
 setup (
     name='sensor',
@@ -13,6 +31,6 @@ setup (
     # author_email='your_email@example.com',
     author_email='priyanshporwal2004@gmail.com',
     packages=find_packages(),
-    install_requires=["pymongo"],
+    # install_requires=["pymongo"],
     install_requires=get_requirements(),
 )
