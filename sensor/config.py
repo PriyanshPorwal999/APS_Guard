08e09aa46160ba59defd58a80db2b2fd3c07d4c9
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2026-01-17 10:42:55 +0530

    Add check for MongoDB URL

--- sensor/config.py
+++ sensor/config.py
@@ -3,11 +3,14 @@ import os
 import pymongo
 
 @dataclass
-
 class EnvironmentVariable:
-    mongo_db_url:str=os.getenv("MONGO_DB_URL")
-
+    mongo_db_url: str = os.getenv("MONGO_DB_URL")
 
 env_var = EnvironmentVariable()
 
-mongo_client = pymongo.MongoClient(env_var.mongo_db_url)    
+if env_var.mongo_db_url is None:
+    raise Exception("MONGO_DB_URL is not set or .env file not loaded")
+
+print("Mongo URL Loaded:", env_var.mongo_db_url)
+
+mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
